d87f3e10c0f723ae7971ea4d2c866dcc1d54c9f6
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2026-01-19 14:19:15 -0300

    feat(drawer navigation): add custom drawer when authenticate UI

---
+++ src/app/(screens)/(drawer)/archiveDashboard.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export default function Archive() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text>archiveDashboard</Text>
+    </View>
+  )
+}
+
